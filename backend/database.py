bc3dff277dd8482995fcd57836e33a7c8f7d8401
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-03-22 23:47:28 +0200

    Updates

---
+++ backend/database.py
@@ -0,0 +1,10 @@
+from sqlmodel import SQLModel, create_engine
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+database_url = os.getenv()
+if not database_url:
+    raise ValueError("Check your .env file")
+engine = create_engine(database_url)
+SQLModel.metadata.create_all(engine)
