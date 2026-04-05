37fb737bf4b57f881a5237ffa2c840aa5be8d75a
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-04-05 14:08:34 +0300

    updates

--- backend/config.py
+++ backend/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 # When my app starts: look in .env
 
 
-class Setting(BaseSettings):
+class Settings(BaseSettings):
     # Database
     database_url: str
 
@@ -20,6 +20,7 @@ class Setting(BaseSettings):
     # LLM
     gemini_api_key: str
 
-
-settings = Setting()
 # If All found, start it, if any missing, show error
+
+
+settings = Settings()
