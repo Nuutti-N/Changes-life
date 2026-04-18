e7858299713f5f70d56e87dfa3d832d7fcc60741
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-04-19 00:14:32 +0300

    add route for frontend pages

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -5,9 +5,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/analyze" element={<Analyze />} />
+        <Route path="/history" element={<History />} />
+
 
       </Routes>
     </BrowserRouter>)
 }
+
 
 export default App
