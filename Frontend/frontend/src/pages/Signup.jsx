a011fd942bd4cd3795881d880a6d2e08b6856046
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-04-20 17:06:14 +0300

    add to users username and password place to register

--- Frontend/frontend/src/pages/Signup.jsx
+++ Frontend/frontend/src/pages/Signup.jsx
@@ -3,10 +3,25 @@ import api from "../api/client"
 import { useNavigate } from "react-router-dom"
 
 function Signup() {
-    return <h1>Sign up</h1>
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    return (
+        <form>
+            <input
+                type="text"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+            />
+            <input
 
+                type="password"
+                value={password}
+                onChange={e => setUsername(e.target.value)}
+            />
+            <button type="submit">Sign up</button>
+        </form>
 
-
+    )
 }
 
 export default Signup
