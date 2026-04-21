85125ad68b2bffda0358e81136cba26c1b3324bd
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-04-21 22:56:17 +0300

    add Loading and error, that users see, what history page give

--- Frontend/frontend/src/pages/History.jsx
+++ Frontend/frontend/src/pages/History.jsx
@@ -8,14 +8,29 @@ function History() {
     const [error, setError] = useState("")
     useEffect(() => {
         async function loadHistory() {
-            const response = await api.get("/history")
-            setHistory(response.data)
-
+            setLoading(true)
+            try {
+                setError("")
+                const response = await api.get("/history")
+                setHistory(response.data)
+            } catch (err) {
+                setError("History not found.")
+            }
+            finally {
+                setLoading(false)
+            }
         }
         loadHistory()
     }, [])
     return (<> <h1>History</h1>
         {history.map(item => <p key={item.id}>{item.claim}</p>)}
+        {
+            error &&
+            <p style={{ color: "red" }}>{error}</p>
+        }
+        {loading &&
+            <p disabled={loading}>{loading ? "Loading..." : "Loading"}</p>
+        }
     </>
     )
 
